f6abeb5a873014d05a926cb9f492d879a0d40190
prajwalsatannavar623 <prajwalsatannavar25@gmail.com>
2026-06-30 02:00:45 +0530

    bug fix

--- server/src/app.js
+++ server/src/app.js
@@ -15,6 +15,8 @@ app.use(
   }),
 );
 
+app.set("trust proxy", 1);
+
 // in-built middlewares
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
